c48aceb06b9879a36b33fc7f307d00923802493c
Ivan Nikolaev <i.b.nikolaev@gmail.com>
2011-05-16 15:44:01 +0700

    7th energy variant and 7th selection varioant

--- share/first-scan.cpp
+++ share/first-scan.cpp
@@ -197,9 +197,12 @@ void track_number(void)
 
 void set_alias(TTree * t)
 {
-  t->SetAlias("ecut", "mdc.E>=0.05");
-  t->SetAlias("ngt","Sum$(mdc.E>=0.05)");
-  t->SetAlias("ngt_Eemc","Sum$((mdc.E>=0.05)*E)");
+  //t->SetAlias("ecut", "mdc.E>=0.05");
+  //t->SetAlias("ngt","Sum$(mdc.E>=0.05)");
+  //t->SetAlias("ngt_Eemc","Sum$((mdc.E>=0.05)*E)");
+
+  //t->SetAlias("atheta","theta[0]+theta[1]  - 3.1415926535");
+  //t->SetAlias("aphi",  "abs(phi[0]-phi[1]) - 3.1415926535");
 }
 
 
@@ -380,39 +383,47 @@ void make_scan_points(vector <ScanPoint_t> &pv)
   //read_energy("share/cbs-energy2.txt",pvtmp);
   //read_energy("share/cbs-energy3.txt",pvtmp);
   //read_energy("share/cbs-energy4.txt",pvtmp);
-  read_energy("share/cbs-energy5.txt",pvtmp);
-  if(false)
+  //read_energy("share/cbs-energy5.txt",pvtmp);
+  //read_energy("share/cbs-energy6.txt",pvtmp);
+  read_energy("share/cbs-energy7.txt",pvtmp);
+  int var=2; //combine
+  int idx=0;
+  switch(var)
   {
-    cout << "Combining first two run" << endl;
-    /*  Combine first points number 1 */
-    pv.resize(pvtmp.size()-1);
-    pv[0]=pvtmp[0];
-    //merge list of runs
-    pv[0].runs.merge(pvtmp[1].runs);
-    pv[0].lum+=pvtmp[1].lum; //integrated luminosity
-    ibn::averager <double> Ea,Ee,Ep;
-    Ea.add(pvtmp[0].W, 1./sq(pvtmp[0].dW)*pvtmp[0].lum);
-    Ea.add(pvtmp[1].W, 1./sq(pvtmp[1].dW)*pvtmp[1].lum);
-    Ee.add(pvtmp[0].Ee, 1./sq(pvtmp[0].dEe)*pvtmp[0].lum);
-    Ee.add(pvtmp[1].Ee, 1./sq(pvtmp[1].dEe)*pvtmp[1].lum);
-    Ep.add(pvtmp[0].Ep, 1./sq(pvtmp[0].dEp)*pvtmp[0].lum);
-    Ep.add(pvtmp[1].Ep, 1./sq(pvtmp[1].dEp)*pvtmp[1].lum);
-    pv[0].W = Ea.average();
-    pv[0].dW=Ea.sigma_average();
-    pv[0].Ee = Ee.average();
-    pv[0].dEe=Ee.sigma_average();
-    pv[0].Ep = Ep.average();
-    pv[0].dEp=Ep.sigma_average();
-    unsigned i2=1;
-    for(unsigned i=2;i<pvtmp.size();++i,++i2) pv[i2] = pvtmp[i];
-    print_data(pv);
-  }
-  else 
-  {
-    pv.resize(pvtmp.size());
-    int idx=0;
-    for(unsigned i=0;i<pvtmp.size();++i,++idx) pv[idx] = pvtmp[i];
-  }
+    case 0:
+      pv.resize(pvtmp.size());
+      for(unsigned i=0;i<pvtmp.size();++i,++idx) pv[idx] = pvtmp[i];
+      break;
+    case 1:
+      pv.resize(pvtmp.size()-1);
+      for(unsigned i=1;i<pvtmp.size();++i,++idx) pv[idx] = pvtmp[i];
+      break;
+    case 2:
+      cout << "Combining first two run" << endl;
+      /*  Combine first points number 1 */
+      pv.resize(pvtmp.size()-1);
+      pv[0]=pvtmp[0];
+      //merge list of runs
+      pv[0].runs.merge(pvtmp[1].runs);
+      pv[0].lum+=pvtmp[1].lum; //integrated luminosity
+      ibn::averager <double> Ea,Ee,Ep;
+      Ea.add(pvtmp[0].W, 1./sq(pvtmp[0].dW)*pvtmp[0].lum);
+      Ea.add(pvtmp[1].W, 1./sq(pvtmp[1].dW)*pvtmp[1].lum);
+      Ee.add(pvtmp[0].Ee, 1./sq(pvtmp[0].dEe)*pvtmp[0].lum);
+      Ee.add(pvtmp[1].Ee, 1./sq(pvtmp[1].dEe)*pvtmp[1].lum);
+      Ep.add(pvtmp[0].Ep, 1./sq(pvtmp[0].dEp)*pvtmp[0].lum);
+      Ep.add(pvtmp[1].Ep, 1./sq(pvtmp[1].dEp)*pvtmp[1].lum);
+      pv[0].W = Ea.average();
+      pv[0].dW=Ea.sigma_average();
+      pv[0].Ee = Ee.average();
+      pv[0].dEe=Ee.sigma_average();
+      pv[0].Ep = Ep.average();
+      pv[0].dEp=Ep.sigma_average();
+      unsigned i2=1;
+      for(unsigned i=2;i<pvtmp.size();++i,++i2) pv[i2] = pvtmp[i];
+      print_data(pv);
+      break;
+  };
 }
 
 
@@ -430,6 +441,8 @@ void make_result(void)
   TCut hp_cut;
   TCut rv_cut;
   TCut ip_cut[3]; //interaction point cut
+  TCut mdcEcut;
+  TCut ggEcut;
 
   //Cut used in selection;
   TCut  mh_cut;
@@ -437,7 +450,7 @@ void make_result(void)
   TCut  gg_cut;
 
 
-  int SELECTION_VERSION=4;
+  int SELECTION_VERSION=7;
   switch(SELECTION_VERSION)
   {
     case 3:
@@ -457,44 +470,103 @@ void make_result(void)
       //ee_cut = ee_ext_cut  && rv_cut && ee_theta_cut;
       /* very strict cut */
       //mh_cut = mh_strict_cut && rv_cut && mh_theta_cut && "emc.ntrack==0";
+      mdcEcut = "Sum$(E>0.05)==nemc";
+      ggEcut = "Sum$(E>0.05)==2";
 
-      mh_cut = mh_base_cut && rv_cut;
-      ee_cut = ee_base_cut && rv_cut &&  ee_theta_cut;
-      gg_cut = gg_base_cut  && gg_theta_cut;
+      //mh_cut = mh_base_cut && rv_cut;
+      //ee_cut = ee_base_cut && rv_cut &&  ee_theta_cut;
+      //gg_cut = gg_base_cut  && gg_theta_cut && ggEcut;
 
       /* new test cut for E cut */
-      //mh_cut = "Sum$(E>0.02)>=3 && S>=0.06 && Eemc<2.5 && Emdc<5"  && rv_cut && "pt100" && mh_theta_cut && "emc.ntrack==0";
-      //ee_cut = "Sum$(E>0.02)==2 && S<=0.05 && Emdc<5 && Eemc>2.5"  && rv_cut && ee_theta_cut && "emc.ntrack==0";
-      //gg_cut = gg_base_cut  && gg_theta_cut && "Sum$(E>0.02)==2";
+      //mh_cut = "Sum$(E>0.02)>=3 && S>=0.06 && Eemc<2.5 && Emdc<5"  && rv_cut;
+      //ee_cut = "Sum$(E>0.02)==2 && S<=0.05 && Emdc<5 && Eemc>2.5"  && rv_cut && ee_theta_cut;
+      gg_cut = gg_base_cut  && gg_theta_cut && "Sum$(E>0.02)==2";
 
-      //mh_cut = "ngt > 2 &&  S>=0.06 && ngt_Eemc<2.5 && Emdc<5" && rv_cut;
+      mh_cut = "ngt > 2 &&  S>=0.06 && ngt_Eemc<2.5 && Emdc<5" && rv_cut && "nemc==ntrack";
+      //mh_cut = "nemc > 2 &&  S>=0.06 && Eemc<2.5 && Emdc<5" && rv_cut && mdcEcut;
       //mh_cut = ("ngt > 2 &&  S>=0.06" || ( "ngt==2 && S>=0.06" && rv_cut && mh_theta_cut)) && "ngt_Eemc<2.5 && Emdc<5";
-      //ee_cut = "ngt == 2 &&  S<=0.05 && ngt_Eemc>2.5 && Emdc<5" && ee_theta_cut && rv_cut;
+      ee_cut = "ngt == 2 &&  S<=0.05 && ngt_Eemc>2.5 && Emdc<5" && ee_theta_cut && rv_cut;
+      //ee_cut = "nemc == 2 &&  S<=0.05 && Eemc>2.5 && Emdc<5" && ee_theta_cut && rv_cut &&mdcEcut;
 
       //strict cut
       //mh_cut = "ngt >= 4  &&  S>=0.06 && ngt_Eemc<2.5 && Emdc<5" && rv_cut && mh_theta_cut && "pt100";
       //ee_cut = "ngt == 2  &&  S<=0.05 && ngt_Eemc>2.5 && Emdc<5" && ee_theta_cut && rv_cut && "emc.ntrack==0";
-      gg_cut = gg_base_cut  && gg_theta_cut;
+      //gg_cut = gg_base_cut  && gg_theta_cut;
       break;
     case 4:
-      mh_base_cut = "mdc.ntrack>2 && S>0.06 && Eemc<2.5 && Emdc<4";
-      mh_theta_cut  = "Sum$(sin(theta)>0.45)==mdc.ntrack";
-      ee_base_cut = "mdc.ntrack==2 && S<0.05 && Eemc>2.5 && Emdc<5";
-      ee_theta_cut = "Sum$(sin(theta)<0.45)==mdc.ntrack";
-      gg_base_cut = "Etotal > 3.3 && Etotal < 4  && sqrt((Sum$(x)-2)**2 + Sum$(y)**2)<4 && abs(Sum$(z))<9";
-      gg_theta_cut = "Sum$(theta)>0.45";
-      TCut gg_cos_cut = "acos(cos)>3.1";
-
-      for(int i=0;i<3;i++)
       {
-        char buf[1024];
-        double IP_R=0.5;//cm
-        double IP_Z=5;//cm
-        sprintf(buf,"rvxy[%d]<%f&&abs(rvz[%d])<%f", i, IP_R, i, IP_Z);
+        mh_base_cut = "mdc.ntrack>2 && S>0.06 && Eemc<2.5 && Emdc<4 && pt50";
+        mh_strict_cut = "mdc.ntrack>3 && S>0.06 && Eemc<2.5 && Emdc<4 && pt100";
+        mh_theta_cut  = "Sum$(sin(theta)>0.45)==mdc.ntrack";
+        ee_base_cut = "mdc.ntrack==2 && S<0.05 && Eemc>2.5 && Emdc<5";
+        ee_theta_cut = "Sum$(sin(theta)<0.45)==mdc.ntrack";
+        gg_base_cut = "Etotal > 3.3 && Etotal < 4  && sqrt((Sum$(x)-2)**2 + Sum$(y)**2)<4 && abs(Sum$(z))<9";
+        gg_theta_cut = "Sum$(theta)>0.45";
+        TCut gg_cos_cut = "acos(cos)>3.1";
+        //TCut tof_cut = "Sum$(tof>0)==tof.tof.ntrack && Sum$(tof<10)==tof.tof.ntrack";
+        //TCut tof_cut = "Sum$(t0>540 && t0<660)==Length$(t0)";
+        TCut tof_cut = "Sum$(tof>1&&tof<6)==Length$(tof) && Sum$(t0>540 && t0<640)==Length$(t0)";
+
+        for(int i=0;i<3;i++)
+        {
+          char buf[1024];
+          double IP_R=0.5;//cm
+          double IP_Z=5;//cm
+          sprintf(buf,"rvxy[%d]<%f&&abs(rvz[%d])<%f", i, IP_R, i, IP_Z);
+          ip_cut[i]=TCut(buf);
+        }
+        //mh_cut = mh_base_cut  && ip_cut[0] && ip_cut[1] && ip_cut[2]&& mh_theta_cut;
+        mh_cut = mh_base_cut  && "Sum$(mdc.rvxy<0.5)==mdc.ntrack && Sum$(abs(mdc.rvz)<5)==mdc.ntrack" && mh_theta_cut;
+        ee_cut = ee_base_cut  && ip_cut[0] && ip_cut[1] && ee_theta_cut && "q[0]*q[1]<0";
+        gg_cut = gg_base_cut  && gg_theta_cut &&gg_cos_cut;
       }
-      mh_cut = mh_base_cut  && ip_cut[0] && ip_cut[1] && ip_cut[2] && mh_theta_cut && "Sum$(q)==0";
-      ee_cut = ee_base_cut  && ip_cut[0] && ip_cut[1] && ee_theta_cut && "q[0]*q[1]<0";
-      gg_cut = gg_base_cut  && gg_theta_cut &&gg_cos_cut;
+      break;
+
+    case 6:
+      {
+        TCut good_track = "Sum$(abs(cos(mdc.theta))<0.93)==mdc.ntrack && Sum$(mdc.rvxy<1)==mdc.ntrack";
+        mh_base_cut  =   "mdc.ntrack>3" && good_track;
+        mh_cut = mh_base_cut;
+
+        TCut ee_acol =   "abs(atheta)<0.03 && -0.06 < aphi && aphi<0.01";
+        TCut ee_endcup = "abs(cos(mdc.theta[0]))>0.86 && abs(cos(mdc.theta[1]))>0.86";
+        TCut ee_E =      "mdc.E[0]/1.843>0.8 && mdc.E[1]/1.843>0.8 && Emdc<5";
+        ee_base_cut = "mdc.ntrack>1 && mdc.ntrack<4" && good_track && ee_endcup && ee_acol && ee_E;
+        ee_cut = ee_base_cut  && "q[0]*q[1]<0";
+
+        TCut gg_acol = "abs(atheta) < 0.05 &&  aphi>-0.08 && aphi<0.04";
+        TCut gg_barel = "Sum$(cos(theta)<0.8)";
+        TCut gg_E = "E[0]/1.843>0.8 && E[1]/1.843>0.8";
+        gg_base_cut = gg_acol && gg_barel && gg_E;
+        gg_cut = gg_base_cut;
+      }
+      break;
+    case 7:
+      {
+        TCut good_track = "Sum$(abs(cos(mdc.theta))<0.93)==mdc.ntrack && Sum$(mdc.rvxy<1)==mdc.ntrack";
+        TCut mh_p = "Sum$(p<2.5)==mdc.ntrack";
+        TCut mh_2good = "mdc.rvxy[0]<1 && mdc.rvxy[1]<1&& Sum$(abs(cos(mdc.theta))<0.93)==mdc.ntrack";
+        TCut mh_2good2 = "mdc.rvxy[0]<1 && mdc.rvxy[1]<1&& abs(cos(mdc.theta[0]))<0.93 &&abs(cos(mdc.theta[1]))<0.93";
+        mh_base_cut  =   "mdc.ntrack>3" && good_track && "S>0.06"&& mh_p;
+        //mh_base_cut  =   "mdc.ntrack>2" && good_track; //week cut
+        //mh_base_cut  =   "mdc.ntrack>4" && good_track && "S>0.06" && "Sum$(cos(theta)<1.8&&pt>0.05)==mdc.ntrack"; //strong cut
+        //mh_base_cut = "mdc.ntrack>2" && "S>0.06" && mh_2good2;
+        mh_cut = mh_base_cut;
+
+        TCut ee_acol =   "abs(atheta)<0.03 && -0.06 < aphi && aphi<0.01";
+        TCut ee_endcup = "abs(cos(mdc.theta[0]))>0.86 && abs(cos(mdc.theta[1]))>0.86";
+        TCut ee_E =      "mdc.E[0]/1.843>0.8 && mdc.E[1]/1.843>0.8 && mdc.E[0]/1.843<1.2 && mdc.E[1]/1.843<1.2";
+        TCut ee_p =  "mdc.p[0]<2.5 && mdc.p[1]<2.5 && mdc.p[0]/1.843>0.9 && mdc.p[1]/1.843>0.9";
+        ee_base_cut = "mdc.ntrack>1 && mdc.ntrack<4" && good_track && ee_endcup && ee_acol && ee_E && ee_p;
+        ee_cut = ee_base_cut  && "q[0]*q[1]<0";
+
+        TCut gg_n = "ngct==0 && ngt>1";
+        TCut gg_acol = "abs(atheta) < 0.05 &&  aphi>-0.06 && aphi<0.02";
+        TCut gg_barel = "cos(theta[0])<0.8 && cos(theta[1])<0.8";
+        TCut gg_E = "E[0]/1.843>0.8 && E[1]/1.843>0.8 && E[0]/1.843<1.2 && E[1]/1.843<1.2";
+        gg_base_cut = gg_acol && gg_barel && gg_E && gg_n;
+        gg_cut = gg_base_cut;
+      }
       break;
   }
   
@@ -533,16 +605,23 @@ void make_result(void)
       TTree * mhadr = (TTree*)file.Get("mhadr");
       TTree * mdc = (TTree*)file.Get("mdc");
       TTree * emc = (TTree*)file.Get("emc");
+      TTree * tof = (TTree*)file.Get("tof");
       mhadr->AddFriend(mdc);
       mhadr->AddFriend(emc);
+      mhadr->AddFriend(tof);
       set_alias(mhadr);
       TTree * gg = (TTree*)file.Get("gg");
+      //gg->SetAlias("atheta","theta[0]+theta[1]-3.1415926535");
+      //gg->SetAlias("aphi","abs(phi[0]-phi[1])-3.1415926535");
       mhadr->Draw("Etotal",mh_cut,"goff");
       unsigned Nsignal = mhadr->GetSelectedRows();
       mhadr->Draw("Etotal",ee_cut,"goff");
       unsigned Nbhabha = mhadr->GetSelectedRows();
       //draw  number of charged tracks
-      mhadr->Draw("mdc.ntrack>>hnchtr",mh_cut && mh_theta_cut && "emc.ntrack==0","goff");
+      mhadr->Draw("mdc.ntrack>>hnchtr",mh_cut,"goff");
+      //TCut nn_cut = "ntrack>2 && Emdc<5&&S<0.05 && S<0.05 && Eemc>2.5&& rvxy[0]<0.5 && rvxy[1]<0.5 && abs(rvz[0]<5 && abs(rvz[1])<5)";
+      TCut nn_cut = mh_cut;
+      mhadr->Draw("mdc.ntrack>>hnchtr", nn_cut,"goff");
       TH1F * hnchtr = (TH1F*)gDirectory->Get("hnchtr");
       hnchtr->Fit("gaus","IQ0");
       TF1 * nchtr_f = hnchtr->GetFunction("gaus");
@@ -556,7 +635,8 @@ void make_result(void)
       //nchtr2_g->SetPointError(runidx,0,hnchtr->GetRMS()/sqrt(mhadr->GetSelectedRows()));
       nchtr = hnchtr->GetMean(); //number of charged tracks
       nchtr_rms = hnchtr->GetRMS()/sqrt(hnchtr->GetEntries()); //RMS of charged tracks.
-      mhadr->Draw("emc.ntrack>>hnntr",mh_cut && mh_theta_cut,"goff");
+      //mhadr->Draw("emc.ntrack>>hnntr",mh_cut ,"goff");
+      mhadr->Draw("emc.ntrack>>hnntr" ,nn_cut,"goff");
       TH1F * hnntr = (TH1F*)gDirectory->Get("hnntr");
       hnntr->Fit("gaus","IQ0");
       //TF1 * nntr_f = hnntr->GetFunction("gaus");
