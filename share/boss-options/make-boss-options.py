85545e6a3adb276d62b4824862f1dd3006a55d9c
Ivan Nikolaev <i.b.nikolaev@gmail.com>
2012-01-17 15:24:45 +0700

    Create all opts.

--- share/boss-options/make-boss-options.py
+++ share/boss-options/make-boss-options.py
@@ -8,7 +8,20 @@ import re
 
 import os
 
-def callback(run, directory, files):
+def configure(source_file_name,  target_file_name,  TEMPLATE_RUN_NUMBER, TEMPLATE_DST_FILES):
+#Open source template files with simulation configuration
+	source_file = open(source_file_name, 'r')
+	target_file = open(target_file_name, 'w')
+	for line in source_file:
+		line = re.sub("TEMPLATE_DST_FILES", TEMPLATE_DST_FILES, line)
+		line = re.sub("TEMPLATE_RUN_NUMBER",TEMPLATE_RUN_NUMBER, line)
+		target_file.write(line)
+	source_file.close()
+	target_file.close()
+
+def proceed(run, directory, files):
+    TEMPLATE_RUN_NUMBER=str(run)
+    TARGET_FILE = TEMPLATE_RUN_NUMBER+".cfg"
     r = "run_0+"+str(run)+"_.+\.dst"
     flist = []
     TEMPLATE_DST_FILES=''
@@ -19,14 +32,15 @@ def callback(run, directory, files):
         if TEMPLATE_DST_FILES=='': comma=''
         else: comma=',\n'
         TEMPLATE_DST_FILES=TEMPLATE_DST_FILES+comma+'"'+name+'"'
+    if TEMPLATE_DST_FILES=='': return
     print TEMPLATE_DST_FILES
+    configure('template.txt',TARGET_FILE,TEMPLATE_RUN_NUMBER, TEMPLATE_DST_FILES)
 
-run = 25201
-print "Proceeding run ", run
-os.path.walk("/home/nikolaev/work/BES/test", callback, run)
+for run in range(25244,25337):
+  #filename = str(run)+".sh"
+  #f = open(filename, 'w')
+  #f.write("cd  $PSIP_BATCH\n")
+  #f.write("boss.exe boss-options/"+str(run)+".cfg\n")
+  print "Proceeding run ", run
+  os.path.walk("/home/nikolaev/work/BES/test", proceed, run)
 
-#for run in range(0025201,0025202):
-#  filename = str(run)+".sh"
-#  f = open(filename, 'w')
-#  f.write("cd  $PSIP_BATCH\n")
-#  f.write("boss.exe boss-options/psip-"+str(run)+".txt\n")
