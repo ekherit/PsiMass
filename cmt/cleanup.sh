b675e28e12be757c62a008890373baaf673e3d22
Ivan Nikolaev ( lxslc.ihep.ac.cn ) <I.B.Nikolaev@inp.nsk.su>
2016-09-06 17:43:57 +0800

    cmt remove

--- cmt/cleanup.sh
+++
@@ -1,9 +0,0 @@
-if test "${CMTROOT}" = ""; then
-  CMTROOT=/afs/.ihep.ac.cn/bes3/offline/ExternalLib/contrib/CMT/v1r20p20081118; export CMTROOT
-fi
-. ${CMTROOT}/mgr/setup.sh
-tempfile=`${CMTROOT}/mgr/cmt -quiet build temporary_name`
-if test ! $? = 0 ; then tempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt cleanup -sh -pack=JPsi -version=JPsi-00-00-01 -path=/afs/ihep.ac.cn/users/n/nikolaev/workarea/6.6.2 $* >${tempfile}; . ${tempfile}
-/bin/rm -f ${tempfile}
-
